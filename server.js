8a701f7af5c45c20e9e5abce08aaf68d9d369f7a
Станислав Евсеенко <evseenko.stanislav@s-telekom.ru>
2020-11-16 00:59:58 +0300

    edding error handing

--- server.js
+++ server.js
@@ -2,22 +2,27 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
     const body = verifyUrl(req.url);
     res.end(body);
 });
 
 function verifyUrl(url) {
-    let urlDirectory = './public';
-    let urlIndex = 'index.html';
-    let urlAddress;
-    if (url == '/') {
-        urlAddress = urlDirectory + url + urlIndex;
-        return fs.readFileSync(urlAddress);
-    } else {
-        urlAddress = urlDirectory + url;
-        return fs.readFileSync(urlAddress);
+    try {
+        let urlDirectory = './public';
+        let urlIndex = 'index.html';
+        let urlAddress;
+        if (url === '/') {
+            urlAddress = urlDirectory + url + urlIndex;
+            return fs.readFileSync(urlAddress);
+        } else {
+            urlAddress = urlDirectory + url;
+            return fs.readFileSync(urlAddress);
+        }
+    } catch (e) {
+        console.log('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack);
+        return
     }
+    
 }
 
 const port = process.env.PORT || 3000;
