a651e7f9ff2dba16c9331e65c3e71dc017640c6b
Станислав Евсеенко <evseenko.stanislav@s-telekom.ru>
2021-01-04 18:20:49 +0300

    add form and change layout

--- public/script.js
+++ public/script.js
@@ -79,8 +79,24 @@ class BasketList extends List {
     constructor(item, button, basket, basItem) {
         super(item, button, basket, basItem)
         this.sumPrice()
+        this.buttonBasket(button)
     }
 
+    buttonBasket(buttonClass) {
+        const BASKET = document.querySelector('.basket')
+        const BASKET_BTN_WRAP = document.querySelector('.basket-button-wrap')
+        const BASKET_LIST = document.querySelector('.basket-list')
+        const BASKET_SUM = document.querySelector('.basket-sum')
+        const NEW_BUTTON = new buttonClass('Корзина', 'basket', BASKET_BTN_WRAP)
+        const BUTTON = document.querySelector('#btn-basket')
+        BUTTON.addEventListener('click', () => {
+            BASKET_LIST.classList.toggle('basket-block')
+            BASKET_SUM.classList.toggle('basket-block')
+            BASKET.classList.toggle('h-50')
+        })
+        
+
+    }
     sumPrice() {
         let itemsPrice = 0
         this.items.forEach(item => {
@@ -291,5 +307,54 @@ class CreateButton {
     }
 }
 
+class CreateForm {
+    patternName = /[A-ZА-Я][a-zа-яё]+/
+    _patternTel = /\+7\([0-9]{3}\)[0-9]{3}-[0-9]{4}/
+    _patternMail = /[0-9a-z]{2}[.-]*[0-9a-z]{4}@mail\.ru/
+    _buttonClass = null
+
+    constructor(buttonClass) {
+        this._buttonClass = buttonClass
+        this.render()
+    }
+
+    render() {
+        const PLACE_TO_RENDER = document.querySelector('aside')
+        if (PLACE_TO_RENDER) {
+            const DIV = document.createElement('div')
+            DIV.classList.add('form-wrap')
+            DIV.innerHTML = `
+                <form>
+                    <input type="text" id="form-name" placeholder="Иван">
+                    <input type="text" id="form-tel" placeholder="+7(900)000-0000">
+                    <input type="text" id="form-amail" placeholder="mymail@mail.ru">
+                </form>
+            `
+            const BUTTON = new this._buttonClass('Отправить', 'form-submit', DIV)
+            const SUBMIT_BTN = DIV.querySelector('button')
+            SUBMIT_BTN.addEventListener('click', () => {
+                this.validation()
+            })
+            PLACE_TO_RENDER.appendChild(DIV)
+        }
+    }
+
+    validation() {
+        const INPUT_NAME = document.getElementById("form-name")
+        const INPUT_TEL = document.getElementById("form-tel")
+        const INPUT_MAIL = document.getElementById("form-amail")
+        if (INPUT_NAME.value) {
+            console.log(this.patternName.test(INPUT_NAME.value) ? INPUT_NAME.value : 'Ошибка ввода!')
+        }
+        if (INPUT_TEL.value) {
+            console.log(this._patternTel.test(INPUT_TEL.value) ? INPUT_TEL.value : 'Ошибка ввода!')
+        }
+        if (INPUT_MAIL.value) {
+            console.log(this._patternMail.test(INPUT_MAIL.value) ? INPUT_MAIL.value : 'Ошибка ввода!')
+        }
+    }
+}
+
 const BASKET_LIST = new BasketList(GoodsItem, CreateButton, BasketList, BasketItem)
 const GOODS_LIST = new GoodsList(GoodsItem, CreateButton, BASKET_LIST, BasketItem)
+const FORM = new CreateForm(CreateButton)
