fb5262f62488edc4a6f171050c2c4d8daae2a85c
Станислав Евсеенко <evseenko.stanislav@s-telekom.ru>
2021-01-28 21:54:43 +0300

    actual build TEST

--- src/shop.js
+++ src/shop.js
@@ -5,8 +5,9 @@ import BasketItem from './basket-item.js';
 import List from './list.js';
 import GoodsList from './goods-list.js';
 import BasketList from './basket-list';
-import './style.css'
+import './style.css';
 
 const BASKET_LIST = new BasketList( GoodsItem, Button, BasketList, BasketItem );
 const GOODS_LIST = new GoodsList( GoodsItem, Button, BASKET_LIST, BasketItem );
-const FORM = new Form( Button )
+const FORM = new Form( Button );
+console.log('lesson5')
